d1a2f30b48321fe8cf09082acbdf8aa2a3959f5a
tonygnc1 <bootcamp1955@gmail.com>
2019-07-09 20:11:02 -0400

    Update index.html file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,7 +8,7 @@ var questions = [
   "The Statue of Liberty is in NY City.",
   "Betsy Ross wrote the Star Spangled Banner.",
   "The Man from Uncle was really his cousin.",
-  "Anne Margaret is the BOMB.",
+  "Anne Margaret is the BOMB."
 ];
 
 var answers = ["false", "true", "false", "false", "true"];
@@ -18,19 +18,22 @@ var showQuestions;
 
 
 // Upon selecting start, the button will disappear and the question will appear in its place
-  $("#button").click(function (e) {
-    $("$startButton").show();
-    console.log("Works here");
-
-    e.stopPropagation(); // 
-  });
-
-  $(button).click(function (e) {
-    $("$startButton").hide();
-  });
 
 
-  $("#reset").click(resetGame);
+$(document).ready(function() {
+  $("#divShow");
+  $("#myDiv").css("display","block");
+})
+
+$("#divHide").click(function() {
+  $("#myDiv").css("display","none");
+})
+   
+ 
+
+  function showQuestions()
+
+ $("#reset").click(resetGame);
 
   function displayQuestions() {
     $("question-holder").html()
@@ -50,6 +53,7 @@ var showQuestions;
       console.log(incorrectAnswer);
     }
   }
+  
 
   //    When the answer is selected and alert shown, a 2 second timer will hide the question
   // and the next question will be unhid slowly in its place.
@@ -60,6 +64,4 @@ var showQuestions;
 
 
 
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
