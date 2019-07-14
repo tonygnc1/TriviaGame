671489f762b53a77f936ffe31fb2a4fc1b756393
tonygnc1 <bootcamp1955@gmail.com>
2019-07-13 20:05:20 -0400

    Fixed function errors

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,23 +13,23 @@ var questions = [
 
 var answers = ["false", "true", "false", "false", "true"];
 var showQuestions;
-var questionsHolder=[];
-var anwersHolder=[];
+var questionsHolder = [];
+var anwersHolder = [];
 
 
 // Upon selecting start, the button will disappear and the question will appear in its place
 
 
-$(document).ready(function() {
- <a id="button-to-hide" onclick="check()"></a>
+$(document).ready(function () {
+  <a id="button-to-hide" onclick="check()"></a>
 
-$('#button-to-hide').hide();
-   
- 
+  $('#button-to-hide').hide();
+
+})
 
   // function showQuestions()
 
- $("#reset").click(resetGame);
+  $("#reset").click(resetGame);
 
   function displayQuestions() {
     $("questionsHolder").html()
@@ -37,10 +37,10 @@ $('#button-to-hide').hide();
 
   function check() {
     document.getElementById("correctAnswer").checked = true;
-}
-function check() {
+  }
+  function check() {
     document.getElementById("incorrectAnswer").checked = false;
-}
+  }
 
 
   // The selected answer is shown an alert box with Correct or Incorrect Response
@@ -57,16 +57,15 @@ function check() {
       console.log(incorrectAnswer);
     }
   }
-  
+
 
   //    When the answer is selected and alert shown, a 2 second timer will hide the question
   // and the next question will be unhid slowly in its place.
 
 
 
-})
 
 
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0
-    W1" crossorigin="anonymous"></script>
+
+  < script src = "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" ></script >
